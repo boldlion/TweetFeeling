cd9e2cceb46f76162d253049d1114432c18492dc
Bold Lion <boldlion1988@gmail.com>
2019-01-06 12:20:58 +0200

    Add Swifter Framework

--- TweetFeeling/ViewController.swift
+++ TweetFeeling/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var feelLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var textField: UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-
+    @IBAction func predictTapped(_ sender: Any) {
+        
+    }
+    
 }
 
