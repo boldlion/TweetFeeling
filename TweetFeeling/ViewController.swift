47649ddcd27d0e4077c4a85bfe8b67c1e9bb6f80
Bold Lion <boldlion1988@gmail.com>
2019-01-09 12:48:02 +0200

    Refactor code. Add .gitignore

--- TweetFeeling/ViewController.swift
+++ TweetFeeling/ViewController.swift
@@ -19,50 +19,74 @@ class ViewController: UIViewController {
     var tweetsArray = [TweetSentimentClassifierInput]()
     let classifier = TweetSentimentClassifier()
     
-    let swifter = Swifter(consumerKey: "cw1XBcTqJYLWh2UxG0pn5UDQJ", consumerSecret: "PH7aBoZBUE93wzLgICbtkjC6bOizp9lbEy2eAMRTPqzeAByqXJ")
+    let tweetCount = 100
     
+    var keys: NSDictionary?
+    
+    var swifter: Swifter!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setSwifterApiKeys()
     }
 
-    @IBAction func predictTapped(_ sender: Any) {
-        if let searchText = textField.text, searchText != "" {
-            swifter.searchTweet(using: searchText, lang: "en", count: 100, tweetMode: .extended, success: { results, metadata in
-                for i in 0..<100 {
-                    if let tweet = results[i]["full_text"].string {
-                        let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
-                        self.tweetsArray.append(tweetForClassification)
-                    }
-                }
-                do {
-                    let predictions = try self.classifier.predictions(inputs: self.tweetsArray)
-                    var score = 0
-                    for prediction in predictions {
-                        let opinion = prediction.label
-                        if opinion == "Pos" {
-                            score += 1
-                        }
-                        else if opinion == "Neg" {
-                            score -= 1
-                        }
-                    }
-                    print("Score is: ", score)
-                    self.updateFeelLabel(score: score)
-                    self.textField.text = ""
-                    score = 0
-                }
-                catch {
-                    print(error.localizedDescription)
-                }
-            }, failure: { error in
-                print("There was an error with Twitter Api Request", error.localizedDescription)
-            })
+    fileprivate func setSwifterApiKeys() {
+        if let path = Bundle.main.path(forResource: "SecretApi", ofType: "plist") {
+            keys = NSDictionary(contentsOfFile: path)
+        }
+        if let dict = keys {
+            guard let consumerKey = dict["ConsumerKey"] as? String else { fatalError("Consumer key was not set")}
+            guard let consumerSecret = dict["ConsumerSecret"] as? String else { fatalError("Consumer secret was not set")}
+
+            swifter = Swifter(consumerKey: consumerKey, consumerSecret: consumerSecret)
         }
     }
     
-    func updateFeelLabel(score: Int) {
+    @IBAction func predictTapped(_ sender: Any) {
+        if let searchText = textField.text, searchText != "" {
+            getTweets(with: searchText)
+        }
+    }
+    
+    func getTweets(with searchText: String) {
+        swifter.searchTweet(using: searchText, lang: "en", count: tweetCount, tweetMode: .extended, success: { results, metadata in
+            for i in 0..<self.tweetCount {
+                if let tweet = results[i]["full_text"].string {
+                    let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                    self.tweetsArray.append(tweetForClassification)
+                }
+            }
+            self.makePredictions()
+        }, failure: { error in
+            print("There was an error with Twitter Api Request", error.localizedDescription)
+        })
+    }
+    
+    func makePredictions() {
+        do {
+            let predictions = try self.classifier.predictions(inputs: self.tweetsArray)
+            var score = 0
+            for prediction in predictions {
+                let opinion = prediction.label
+                if opinion == "Pos" {
+                    score += 1
+                }
+                else if opinion == "Neg" {
+                    score -= 1
+                }
+            }
+            self.updateUI(score: score)
+            self.textField.text = ""
+            score = 0
+        }
+        catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func updateUI(score: Int) {
         if score > 20 {
-            feelLabel.text = "🥰"
+            feelLabel.text = "😍"
         } else if score > 10 {
             feelLabel.text = "🙃"
         } else if score > 0 {
